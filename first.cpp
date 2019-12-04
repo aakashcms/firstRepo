9bd7762a8e9818640d39469f36463ed2c205746f
aakashcms <58433441+aakashcms@users.noreply.github.com>
2019-12-04 10:29:25 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,11 @@
+#include<iostream.h>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	cout<<"enter two no:";
+	cin>>a>>b;
+	cout<<"Addition of two "<<a<<" and "<<b<<" is "<<(a+b);
+	return 0;
+}
